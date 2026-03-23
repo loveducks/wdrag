0f51bef75a955c6a74b760ac2074e2cc120ab38a
loveducks <loveducks@clock.li>
2026-03-23 16:15:28 +0530

    added a menu

--- pong.c
+++ pong.c
@@ -1,11 +1,31 @@
 #include <stdbool.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <ncurses.h>
+#include <menu.h>
+#include <string.h>
+
+#define ARRAY_SIZE(a) (sizeof (a) / sizeof(a[0]))
+#define CTRLD 4
+
+typedef struct{
+	char txt[CTRLD+1];
+} AppState;
+
+AppState app_state;
+
+const char *choices[] = {
+	"1. Write",
+	"2. Cancel",
+	"3. Exit",
+};
 
 WINDOW *create_newwin(int height, int width, int starty, int startx);
 void destroy_win(WINDOW *local_win);
 void mouse_mon(int height, int width, int starty, int startx, WINDOW *my_win);
 bool check_coll(int height, int width, int starty, int startx, int m_x, int m_y);
+void spawn_menu();
+void handle_ip(int size);
 
 int main(void) {
 	WINDOW *my_win;
@@ -33,6 +53,72 @@ int main(void) {
 	return 0;
 }
 
+void handle_ip(int size){
+	char c;
+	int i = 0;
+
+	move(1, 1);
+
+	while(i < size) {
+		c = getch();
+		app_state.txt[i++] = c;
+	}
+
+	app_state.txt[i] = '\0';
+}
+
+void spawn_menu(){
+	ITEM **my_items;
+	MENU *my_menu;
+	int c;
+	int n_choices, i;
+	n_choices = ARRAY_SIZE(choices);
+	my_items = (ITEM **) calloc((size_t) (n_choices + 1), sizeof(ITEM *));
+
+	for (int i=0; i<n_choices; ++i)
+	{
+		my_items[i] = new_item(choices[i], "");
+	}
+
+	my_items[n_choices] = (ITEM *) NULL;
+
+	my_menu = new_menu((ITEM **) my_items);
+	set_menu_mark(my_menu, " * ");
+	set_current_item(my_menu, my_items[0]);
+	post_menu(my_menu);
+	refresh();
+
+	while (((c = getch()) != '\n')) {
+		switch(c){
+			case KEY_DOWN:
+				menu_driver(my_menu, REQ_DOWN_ITEM);
+				break;
+			case KEY_UP:
+				menu_driver(my_menu, REQ_UP_ITEM);
+				break;
+			default:
+				break;
+		}
+	}
+	ITEM* cur = current_item(my_menu);
+	const char* name = item_name(cur);
+
+	if (strcmp(name, "1. Write") == 0) {
+		handle_ip(CTRLD);
+	}
+	mvprintw(0, 0, "");
+	refresh();
+	unpost_menu(my_menu);
+	free_menu(my_menu);
+
+	for (int i=0; i<n_choices; ++i)
+	{
+		free_item(my_items[i]);
+	}
+
+	free(my_items);
+}
+
 bool check_coll(int height, int width, int starty, int startx, int m_x, int m_y) {
 	if (
 		startx <= m_x && m_x <= startx + width &&
@@ -48,6 +134,8 @@ void mouse_mon(int height, int width, int starty, int startx, WINDOW *my_win) {
 
 	mousemask(ALL_MOUSE_EVENTS | REPORT_MOUSE_POSITION, NULL);
 	printf("\033[?1003h\n"); // Makes the terminal report mouse movement events
+	int start_x_init = startx;
+	int start_y_init = starty;
 
 	bool move = false;
 
@@ -56,9 +144,6 @@ void mouse_mon(int height, int width, int starty, int startx, WINDOW *my_win) {
 
 		if (c == '\n')
 			break;
-
-		char buffer[512];
-		size_t max_size = sizeof(buffer);
 
 		if (c == KEY_MOUSE) {
 			if (getmouse(&event) == OK) {
@@ -78,10 +163,19 @@ void mouse_mon(int height, int width, int starty, int startx, WINDOW *my_win) {
 					mvwin(my_win, starty, startx);
 				}
 
-				mvprintw(0, 0, "x=%d y=%d dragging=%d", event.x, event.y, move);
+				//mvprintw(0, 0, "x=%d y=%d dragging=%d", event.x, event.y, move);
 			}
 		}
 
+		if (c == 'M' || c == 'm') {
+			move = false;
+			spawn_menu();
+			refresh();
+			destroy_win(my_win);
+			my_win = create_newwin(height, width, starty, startx);
+		}
+
+		mvprintw(starty + 1, startx + 1, "%s", app_state.txt);
 		wrefresh(my_win);
 		refresh();
 
@@ -106,8 +200,8 @@ void destroy_win(WINDOW *local_win) {
 	 * result of erasing the window. It will leave its four corners
 	 * and so an ugly remnant of window. (which is kind of what we want, but not needed anymore)
 	*/
-	box(local_win, ' ', ' ');
-	// wborder(local_win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+	// box(local_win, ' ', ' ');
+	wborder(local_win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
 	wrefresh(local_win);
 	delwin(local_win);
 }
